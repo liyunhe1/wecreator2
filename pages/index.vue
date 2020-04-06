3ad3331cfdaa5a9cca2f51061815cc132c746e6c
liyunhe <13718567030@139.com>
2020-04-07 01:18:40 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="margin: 20px">
+    <input
+      class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+      type="email"
+      placeholder="jane@example.com"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style></style>
